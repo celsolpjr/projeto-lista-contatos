85b0fb92cf95a38ec7c493a14e3a0cf11e8b639b
celsojr <celso.8882011@gmail.com>
2025-08-21 20:57:11 -0300

    funcao inserir_contato finalizada

--- lista_contatos.c
+++ lista_contatos.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char nome[50];
@@ -9,7 +11,7 @@ void inserir_contato(Contato **ptr_contatos, int *tamanho);
 
 int main()
 {
-    Contato *contatos;
+    Contato *contatos = NULL;
     int opcao = 0, tamanho = 0;
 
     while(opcao != 5) {
@@ -18,14 +20,16 @@ int main()
         puts("3 - Buscar um contato");
         puts("4 - Remover um contato");
         puts("5 - Sair");
-        printf("Digite uma opcao: ");
+        printf("\nDigite uma opcao: ");
         scanf("%d", &opcao);
+        getchar();
 
         switch(opcao) {
             case 1:
                 inserir_contato(&contatos, &tamanho);
                 break;
             case 5:
+                free(contatos);
                 return 0;
             default:
                 printf("Opcao invalida!\n\n");
@@ -39,10 +43,26 @@ int main()
 void inserir_contato(Contato **ptr_contatos, int *tamanho) {
     char nome[50], telefone[20];
 
-    printf("Digite o nome: ");
-    scanf("%s", nome);
-    printf("Digite o telefone: ");
-    scanf("%s", telefone);
+    printf("\nDigite o nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0';
 
+    printf("Digite o telefone: ");
+    fgets(telefone, sizeof(telefone), stdin);
+    telefone[strcspn(telefone, "\n")] = '\0';
+
+    Contato *tmp = realloc(*ptr_contatos, ((*tamanho) + 1) * sizeof(Contato));
+
+    if (tmp == NULL) {
+        printf("Erro ao alocar memoria!\n\n");
+        return;
+    }
+    *ptr_contatos = tmp;
     
+    strcpy((*ptr_contatos + *tamanho)->nome, nome);
+    strcpy((*ptr_contatos + *tamanho)->telefone, telefone);
+
+    (*tamanho)++;
+
+    printf("\nContato cadastrado!\n\n");
 }
